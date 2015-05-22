34fcf60d2872c3b0ff910f13494a1e9c1b9614ca
sanket <sanket.parmar@rtcamp.com>
2015-05-22 15:18:17 +0530

    Removed unwanted code

--- app/RTMediaCubePoints.php
+++ app/RTMediaCubePoints.php
@@ -15,8 +15,6 @@ class RTMediaCubePoints {
     
     public function __construct() {
         $this->rtmedia_cubepoints_load_translation();
-        // rtMedia Moderation db upgrade
-		add_action( 'init', array( $this, 'rtmedia_cubepoints_do_upgrade' ), 9, 1 );
     }
     
     /*
@@ -25,23 +23,5 @@ class RTMediaCubePoints {
 	public function rtmedia_cubepoints_load_translation() {
 		load_plugin_textdomain( 'rtmedia', false, basename( RTMEDIA_CUBEPOINTS_PATH ) . '/languages/' );
 	}
-    
-    public function rtmedia_cubepoints_do_upgrade() {
-        if ( class_exists( 'RTDBUpdate' ) ) {
-            // db upgrade for moderation schema
-            $update = new RTDBUpdate( false, RTMEDIA_CUBEPOINTS_PATH . "index.php", false, true );
-            
-            if( !defined( 'RTMEDIA_CUBEPOINTS_VERSION' ) ) {
-                /**
-                 * The version of the plugin
-                 */
-                define( 'RTMEDIA_CUBEPOINTS_VERSION', '1.0' );
-            }
-            
-            if( $update->check_upgrade() ) {
-                $update->do_upgrade();
-            }
-        }
-    }
     
 }
