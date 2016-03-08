f2446a4eee7de7b9fdb613b51429c5ce05f7463e
Sanket <sanket.parmar11@gmail.com>
2016-03-08 17:20:21 +0530

    Added GPLv2 License & Refactor

--- index.php
+++ index.php
@@ -1,51 +1,52 @@
 <?php
 
 /*
-  Plugin Name: rtMedia CubePoints
-  Plugin URI:  https://rtcamp.com/products/rtmedia-cubepoints/
-  Description: This plugin provides CubePoints integration with rtMedia.
-  Version:     1.1.1
-  Author:      rtCamp
-  Text Domain: rtmedia
-  Author URI:  http://rtcamp.com/?utm_source=dashboard&utm_medium=plugin&utm_campaign=rtmedia-cubepoints
-  License:     GPL2
-*/
+ * Plugin Name: rtMedia CubePoints
+ * Plugin URI: https://rtcamp.com/products/rtmedia-cubepoints/
+ * Description: This plugin provides CubePoints integration with rtMedia.
+ * Version: 1.1.1
+ * Text Domain: rtmedia
+ * Author: rtCamp
+ * Author URI: http://rtcamp.com/?utm_source=dashboard&utm_medium=plugin&utm_campaign=rtmedia-cubepoints
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
 
+/**
+ *  The server file system path to the plugin directory
+ */
 if ( ! defined( 'RTMEDIA_CUBEPOINTS_PATH' ) ) {
-	/**
-	 *  The server file system path to the plugin directory
-	 */
 	define( 'RTMEDIA_CUBEPOINTS_PATH', plugin_dir_path( __FILE__ ) );
 }
 
+/**
+ * The url to the plugin directory
+ */
 if ( ! defined( 'RTMEDIA_CUBEPOINTS_URL' ) ) {
-	/**
-	 * The url to the plugin directory
-	 */
 	define( 'RTMEDIA_CUBEPOINTS_URL', plugin_dir_url( __FILE__ ) );
 }
 
+/**
+ * The base name of the plugin directory
+ */
 if ( ! defined( 'RTMEDIA_CUBEPOINTS_BASE_NAME' ) ) {
-	/**
-	 * The base name of the plugin directory
-	 */
 	define( 'RTMEDIA_CUBEPOINTS_BASE_NAME', plugin_basename( __FILE__ ) );
 }
 
+/**
+ * The version of the plugin
+ */
 if ( ! defined( 'RTMEDIA_CUBEPOINTS_VERSION' ) ) {
-	/**
-	 * The version of the plugin
-	 */
 	define( 'RTMEDIA_CUBEPOINTS_VERSION', '1.1.1' );
 }
 
+// this is the URL our updater / license checker pings. This should be the URL of the site with EDD installed
 if ( ! defined( 'EDD_RTMEDIA_CUBEPOINTS_STORE_URL' ) ) {
-	// this is the URL our updater / license checker pings. This should be the URL of the site with EDD installed
 	define( 'EDD_RTMEDIA_CUBEPOINTS_STORE_URL', 'https://rtcamp.com/' );
 }
 
+// the name of your product. This should match the download name in EDD exactly
 if ( ! defined( 'EDD_RTMEDIA_CUBEPOINTS_ITEM_NAME' ) ) {
-	// the name of your product. This should match the download name in EDD exactly
 	define( 'EDD_RTMEDIA_CUBEPOINTS_ITEM_NAME', 'rtMedia CubePoints' );
 }
 
@@ -68,6 +69,7 @@ function rtmedia_cubepoints_autoloader( $class_name ) {
 
 	foreach ( $rtlibpath as $path ) {
 		$path = RTMEDIA_CUBEPOINTS_PATH . $path;
+
 		if ( file_exists( $path ) ) {
 			include $path;
 
@@ -76,43 +78,56 @@ function rtmedia_cubepoints_autoloader( $class_name ) {
 	}
 }
 
-function rtmedia_cubepoints_loader( $class_construct ) {
+/**
+ * Register the autoloader function into spl_autoload
+ */
+spl_autoload_register( 'rtmedia_cubepoints_autoloader' );
 
+/**
+ * @param $class_construct
+ *
+ * @return mixed
+ */
+function rtmedia_cubepoints_loader( $class_construct ) {
 	/*
 	 * do not load classes of rtMedia Pro is activated
 	 * as it might break some functionality
 	 */
 	if ( defined( 'RTMEDIA_PRO_PATH' ) ) {
 		add_action( 'admin_notices', 'rtmedia_pubepoints_pro_active_notice' );
+
 		return $class_construct;
 	}
 
 	require_once RTMEDIA_CUBEPOINTS_PATH . 'app/RTMediaCubePoints.php';
 
-	$class_construct['CubePoints'] = false;
+	$class_construct['CubePoints']      = false;
 	$class_construct['CubePointsMedia'] = false;
 
 	return $class_construct;
 }
+
+add_filter( 'rtmedia_class_construct', 'rtmedia_cubepoints_loader' );
+
+/**
+ * rtMedia Pro notice
+ */
 function rtmedia_pubepoints_pro_active_notice() {
 	?>
-		<div class="error">
-			<p>
-				<strong>rtMedia CubePoints</strong> plugin cannot be activated with rtMedia Pro. Please <strong><a href="https://rtcamp.com/blog/rtmedia-pro-splitting-major-change" target="_blank">read this</a></strong> for more details. You may <strong><a href="https://rtcamp.com/premium-support/" target="_blank">contact support for help</a></strong>.
-			</p>
-		</div>
+	<div class="error">
+		<p>
+			<strong>rtMedia CubePoints</strong> plugin cannot be activated with rtMedia Pro. Please <strong><a
+					href="https://rtcamp.com/blog/rtmedia-pro-splitting-major-change" target="_blank">read
+					this</a></strong> for more details. You may <strong><a href="https://rtcamp.com/premium-support/"
+			                                                               target="_blank">contact support for help</a></strong>.
+		</p>
+	</div>
 	<?php
 	// automatic deactivate plugin if rtMedia Pro is active and current user can deactivate plugin.
 	if ( current_user_can( 'activate_plugins' ) ) {
 		deactivate_plugins( RTMEDIA_CUBEPOINTS_BASE_NAME );
 	}
 }
-
-/**
- * Register the autoloader function into spl_autoload
- */
-spl_autoload_register( 'rtmedia_cubepoints_autoloader' );
-add_filter( 'rtmedia_class_construct', 'rtmedia_cubepoints_loader' );
 
 /*
  * EDD License
