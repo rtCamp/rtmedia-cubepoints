4e80c9c145f0a824d46a62ddb2e7a7275ed35544
Thrijith Thankachan <thrijith13@gmail.com>
2018-01-09 12:52:54 +0530

    Add Settings and Docs links in plugin description

--- index.php
+++ index.php
@@ -165,3 +165,29 @@ global $rtm_plugin_installer;
 if ( empty( $rtm_plugin_installer ) ) {
 	$rtm_plugin_installer = new RTMPluginInstaller();
 }
+
+/**
+ * Add Settings/Docs link to plugins area.
+ *
+ * @since 1.1.3
+ *
+ * @param array  $links Links array in which we would prepend our link.
+ * @param string $file Current plugin basename.
+ *
+ * @return array Processed links.
+ */
+function rtmedia_cubepoints_action_links( $links, $file ) {
+	// Return normal links if not plugin.
+	if ( plugin_basename( __FILE__ ) !== $file ) {
+		return $links;
+	}
+
+	// Add a few links to the existing links array.
+	return array_merge( $links, array(
+		'settings' => '<a href="' . esc_url( admin_url( 'admin.php?page=rtmedia-settings#rtmedia-cubepoints' ) ) . '">' . esc_html__( 'Settings', 'rtmedia' ) . '</a>',
+		'docs'     => '<a target="_blank" href="' . esc_url( 'https://rtmedia.io/docs/addons/cubepoints/' ) . '">' . esc_html__( 'Docs', 'rtmedia' ) . '</a>',
+	) );
+}
+
+add_filter( 'plugin_action_links', 'rtmedia_cubepoints_action_links', 11, 2 );
+add_filter( 'network_admin_plugin_action_links', 'rtmedia_cubepoints_action_links', 11, 2 );
